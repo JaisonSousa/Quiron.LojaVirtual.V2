3ebc160506d22f6084bc084ee4f57c40f99997b3
JaisonSousa <jaison.sp13@gmail.com>
2017-09-09 12:06:32 -0300

    alteração

--- Quiron.LojaVirtual.V2.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.V2.UnitTest/UnitTest1.cs
@@ -1,35 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Quiron.LojaVirtual.V2.UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void Take()
-        //{
-        //    //O operador Take é usado para selecionar os primeiros n objetos de uma coleção
+        [TestMethod]
+        public void Take()
+        {
+            //O operador Take é usado para selecionar os primeiros n objetos de uma coleção 
 
-        //    int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-        //    var resultado = from num in numeros.Take(5) select num;
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5) select num;
+            
+            int[] teste = { 5, 4, 1, 3, 9 };
 
-        //    int[] teste = { 5, 4, 1, 3, 9 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+        }
 
-        //    CollectionAssert.AreEqual(resultado.ToArray(), teste);
-        //}
+        [TestMethod]
+        public void Skip()
+        {
+            //O operador Skip ignora o(s) primeiros(s) n objetos de uma coleção.
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
 
-        //[TestMethod]
-        //public void Skip()
-        //{
-        //    //O operador Skip ignora o(s) primeiros(s) n objetos de uma coleção.
-        //    int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-        //    var resultado = from num in numeros.Take(5).Skip(2) select num;
+            int[] teste = { 1, 3, 9 };
 
-        //    int[] teste = { 1, 3, 9 };
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
 
-        //    CollectionAssert.AreEqual(resultado.ToArray(), teste);
-
-        //}
+        }
     }
 }
