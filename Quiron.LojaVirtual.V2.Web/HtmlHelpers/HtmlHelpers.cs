ef3f408187778cb17d32689067345c3824467b5b
JaisonSousa <jaison.sp13@gmail.com>
2017-09-15 23:15:39 -0300

    Classe paginação

---
+++ Quiron.LojaVirtual.V2.Web/HtmlHelpers/HtmlHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.V2.Web.HtmlHelpers
+{
+    public static class HtmlHelpers
+    {
+        //Representa uma cadeia de caracteres codificada em HTML que não deve ser codificada novamente.
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            //objeto dinâmico que permite que você expanda o número de caracteres na cadeia de caracteres que ele encapsula
+            StringBuilder resultado = new StringBuilder();
+
+            
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
+            {
+                //Representa uma classe que é usada por auxiliares HTML para criar elementos HTML.
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+
+                }
+
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+
+
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+
+    }
+}
---
+++ Quiron.LojaVirtual.V2.Web/Models/Paginacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.V2.Web.Models
+{
+    public class Paginacao
+    {
+        //Quantos itens tenho no banco
+        public int ItensTotal { get; set; }
+        //Quantos itens quero por pagina
+        public int ItensPorPargina { get; set; }
+        //Qual página esta exibida no momento
+        public int PaginaAtual { get; set; }
+
+        //Quantas página tem
+        public int TotalPagina
+        {
+            get { return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPargina); }
+        }
+
+    }
+}
